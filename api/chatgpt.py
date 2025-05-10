fbdfaa6b4a287ccaa950e8c7afafdeb39ef04717
TANAPONG_ <126324685+zgfff@users.noreply.github.com>
2025-05-11 00:48:26 +0700

    Update chatgpt.py

--- api/chatgpt.py
+++ api/chatgpt.py
@@ -1,30 +1,18 @@
-from api.prompt import Prompt
-
-import os
 import openai
+import os
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 class ChatGPT:
     def __init__(self):
-        self.prompt = Prompt()
-        #self.model = os.getenv("OPENAI_MODEL", default = "gpt-3.5-turbo")
-        self.model = os.getenv("OPENAI_MODEL", default = "text-davinci-003")
-        self.temperature = float(os.getenv("OPENAI_TEMPERATURE", default = 0))
-        self.frequency_penalty = float(os.getenv("OPENAI_FREQUENCY_PENALTY", default = 0))
-        self.presence_penalty = float(os.getenv("OPENAI_PRESENCE_PENALTY", default = 0.6))
-        self.max_tokens = int(os.getenv("OPENAI_MAX_TOKENS", default = 240))
+        self.messages = []
+
+    def add_msg(self, msg):
+        self.messages.append({"role": "user", "content": msg})
 
     def get_response(self):
-        response = openai.Completion.create(
-            model=self.model,
-            prompt=self.prompt.generate_prompt(),
-            temperature=self.temperature,
-            frequency_penalty=self.frequency_penalty,
-            presence_penalty=self.presence_penalty,
-            max_tokens=self.max_tokens
+        response = openai.ChatCompletion.create(
+            model="gpt-4",
+            messages=self.messages
         )
-        return response['choices'][0]['text'].strip()
-
-    def add_msg(self, text):
-        self.prompt.add_msg(text)
+        return response.choices[0].message.content.strip()
