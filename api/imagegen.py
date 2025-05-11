abd6e9021f308d6e1f394625bd0a32b85730232f
TANAPONG_ <126324685+zgfff@users.noreply.github.com>
2025-05-11 13:24:29 +0700

    Update imagegen.py

--- api/imagegen.py
+++ api/imagegen.py
@@ -7,6 +7,6 @@ def generate_image(prompt):
     response = openai.Image.create(
         prompt=prompt,
         n=1,
-        size="1024x1024"
+        size="512x512"
     )
     return response['data'][0]['url']
